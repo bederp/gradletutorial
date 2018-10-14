c3bafe82b42289cd44dc67fa7a175fc670aa778d
Paweł Beder <pawel.beder@gmail.com>
2018-10-14 18:11:38 +0200

    Task 4 bumped gradle to version 4.9 so that --args can be used to pass arguments to tasks. Added application plugin. Moved App.java into pl.beder package. Try executing run --args "thing otherthing"

--- src/main/java/pl/beder/App.java
+++ src/main/java/pl/beder/App.java
@@ -1,3 +1,5 @@
+package pl.beder;
+
 import com.google.common.collect.ImmutableMap;
 
 import static com.google.common.collect.ImmutableMap.of;
@@ -7,8 +9,14 @@ public class App {
     private static final String JAVA = "Java";
 
     public static void main(String[] args) {
+        printPassedArguments(args);
         ImmutableMap<String, String> map = of("Java", "Coders School");
-
         System.out.println(map.get(JAVA));
+    }
+
+    private static void printPassedArguments(String[] args) {
+        for (String arg : args) {
+            System.out.println("Passed to main: " + arg);
+        }
     }
 }
